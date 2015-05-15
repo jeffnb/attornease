7b3d36d558e50f5bd3042ef2f3748f574d5f1d24
Jeff <jeff@goproject100.com>
2015-05-15 14:36:35 -0700

    Adding home page

--- attornease/urls.py
+++ attornease/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from attorney.views import IndexPageView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^attorneys/', include('attorney.urls'))
+    url(r'^attorneys/', include('attorney.urls')),
+    url(r'^errors/404', TemplateView.as_view(template_name="404.html"), name="404_error"),
+    url(r'^$', IndexPageView.as_view(), name="index"),
 )
--- attorney/views.py
+++ attorney/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
 
 # Create your views here.
-from django.views.generic import DetailView, ListView
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView, ListView, TemplateView, View
 from attorney.models import Attorney, LawCategory, ServiceArea
 
 
@@ -19,14 +21,22 @@ class AttorneyListView(ListView):
     def get_queryset(self):
         queryset = None
 
-        area_id = int(self.kwargs.get('area_id', 0))
-        cat_id = int(self.kwargs.get('cat_id', 0))
+        area_id = int(self.get_param('area_id', 1))
+        cat_id = int(self.get_param('cat_id', 0))
         if cat_id > 0:
             queryset = Attorney.objects.filter(service_areas__id=area_id, categories__id=cat_id)
         else:
             queryset = Attorney.objects.filter(service_areas__id=area_id)
 
         return queryset
+
+    def get_param(self, key, default):
+        value = default
+        if key in self.request.GET:
+            value = self.request.GET[key]
+        elif key in self.kwargs:
+            value = self.kwargs[key]
+        return value
 
     def get_context_data(self, **kwargs):
         context = super(AttorneyListView, self).get_context_data(**kwargs)
@@ -39,7 +49,21 @@ class AttorneyListView(ListView):
             context['selected_category'] = LawCategory.objects.get(pk=cat_id)
 
         # Grab area id
-        area_id = int(self.kwargs.get('area_id', 0))
+        area_id = int(self.kwargs.get('area_id', 1))
         context['selected_area'] = ServiceArea.objects.get(pk=area_id)
 
         return context
+
+class IndexPageView(View):
+
+    template_name = "home.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {'categories': LawCategory.objects.all(), 'areas': ServiceArea.objects.all()}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        category_id = self.request.POST.get("category_id", 1)
+        area_id = self.request.POST.get("area_id", 1)
+
+        return redirect("attorney_list_cat", area_id=area_id, cat_id=category_id, permanent=True)
